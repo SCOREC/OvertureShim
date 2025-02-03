b401d9972437a8c88eddb1c25fa2c18eb17f9fa3
cartej11 <cartej11@rpi.edu>
2025-02-03 18:07:20 +0000

    Remove primer folder from branch.

--- primer/pwave1.cmd
+++
@@ -1,6 +0,0 @@
-tFinal .2
-tPlot .1
-* save show file 1
-compute
-* 
-exit
--- primer/pwave2.cmd
+++
@@ -1,9 +0,0 @@
-tFinal .4
-tPlot .1
-pulse params 30 3 2 .25 0 (alpha,a0,pulsePow,x0,y0)
-
-save show file 1
-
-compute
-
-exit
